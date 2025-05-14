e82a9487908a37b242bd7549055dbe5be988d4b1
Eledem <eledem1712@gmail.com>
2025-05-14 15:21:38 +0300

    изменено, добавлен докер (лаба 17)

--- pelican-magazine-backend-2025s/WebApplication6/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ pelican-magazine-backend-2025s/WebApplication6/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dee3680b349cce6d5bbe999d3539f29354f07ee7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
