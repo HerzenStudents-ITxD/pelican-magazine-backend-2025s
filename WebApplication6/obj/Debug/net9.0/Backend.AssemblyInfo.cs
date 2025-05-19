abd1fc3291b01b6c36e586ce5573098ac30de94c
Eledem <eledem1712@gmail.com>
2025-05-19 18:59:41 +0300

    обновление

--- WebApplication6/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ WebApplication6/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+705180b7275b60f8563b33f7fef657e64a984ba9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f793288d8a2e20c7aba01a69f4f8c06b9a2c9f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
